58ad665cdd867d065d33d89dbc4d098169939ca1
adatta2025 <arijit.datta2006@gmail.com>
2025-04-23 00:11:46 +0530

    Adding file to practice Jenkins GIT based project

---
+++ Date_Time.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+curr_date = datetime.today()
+print("\nCurrent date and time\t:", curr_date)
+print("\nCurrent year in full\t:", curr_date.strftime("%Y"))
+print("\nMonth of year full name\t:", curr_date.strftime("%B"))
+print("\nWeekday of the week\t:", curr_date.strftime("%w"))
+print("\nDay of the month\t:", curr_date.strftime("%d"))
+print("\nDay of week in full name:", curr_date.strftime("%A"), '\n')
