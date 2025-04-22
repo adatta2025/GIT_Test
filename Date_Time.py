8f87c8e4255cfaf35f3df25231b277e7e22ced0e
adatta2025 <arijit.datta2006@gmail.com>
2025-04-23 00:23:52 +0530

    Added equal to seperator

--- Date_Time.py
+++ Date_Time.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 curr_date = datetime.today()
+print("\n====================================")
 print("\nCurrent date and time\t:", curr_date)
 print("\nCurrent year in full\t:", curr_date.strftime("%Y"))
 print("\nMonth of year full name\t:", curr_date.strftime("%B"))
